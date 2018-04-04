95d1917eedf1341a7a9cdbf07c34a450b4f3bc65
ebey97 <ebey97@gmail.com>
2018-04-04 21:42:55 +0530

    first commit

---
+++ sim.py
@@ -0,0 +1,62 @@
+from threading import Thread
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+import random
+
+class NavigateMap(Thread):
+	def __init__(self,botNum):
+		# initiliaze
+		Thread.__init__(self)
+		self.botNum = botNum
+		
+	def run(self):
+		res = ''
+		rospy.loginfo("bot" + self.botNum + 'started')
+		#rospy.on_shutdown(self.shutdown)
+		#rospy.Subscriber('/robot' + self.botNum + '/laser_' + self.botNum, LaserScan, callback)
+		
+		print('-' + str(res))
+    
+		velTopic = '/robot' + self.botNum + '/cmd_vel'
+       
+		self.cmd_vel = rospy.Publisher(velTopic, Twist, queue_size=10)
+     
+		r = rospy.Rate(10)
+
+		move_cmd = Twist()
+		move_cmd.linear.x = 0.2
+
+		while not rospy.is_shutdown():
+			move_cmd.angular.z = random.randint(-2,2)
+			self.cmd_vel.publish(move_cmd)
+			r.sleep()
+                           
+	def shutdown(self):
+		# stop bot
+		rospy.loginfo("Stop Bot")
+		self.cmd_vel.publish(Twist())
+		rospy.sleep(1)
+		
+def main():
+	try:
+		rospy.init_node('Navigate', anonymous=False, disable_signals = True)
+		
+		bots = []
+		
+		for i in range(3):
+			newBot = NavigateMap(str(i))
+			bots.append(newBot)
+		
+		for i in range(3):
+			bots[i].start()
+		
+		for i in range(3):
+			bots[i].join()
+		
+	except:
+		rospy.loginfo("Node terminated.")
+		
+
+if __name__ == '__main__':
+	main()
